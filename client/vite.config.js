4b9f774a28f53ff19b41f05394ce51d35f5645f7
SumanSaha <sumansahaweb.dev@gmail.com>
2024-12-09 21:06:51 +0530

    updated vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // Add this for path resolution
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Add this alias for better path resolution
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   optimizeDeps: {
     include: ['@emotion/react', '@emotion/styled'],
+  },
+  resolve: {
+    preserveSymlinks: true, // Ensures proper resolution of file paths
   },
 });
