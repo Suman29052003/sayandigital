222c901a8872a990b4dd0164bb0e611253197915
SumanSaha <sumansahaweb.dev@gmail.com>
2024-12-17 22:31:29 +0530

    `Revert changes to baseURL.js, restoring original import statement`

--- client/src/baseURL.js
+++ client/src/baseURL.js
@@ -1,5 +1,5 @@
-// const baseURL = import.meta.env.VITE_BACKENDURI || "http://localhost:8000"
-const baseURL =  "http://localhost:8000"
+const baseURL = import.meta.env.VITE_BACKENDURI || "http://localhost:8000"
+// const baseURL =  "http://localhost:8000"
 
 
 export default baseURL;
